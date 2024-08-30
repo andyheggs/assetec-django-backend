c7089989178f5ff3dc6062bc7ed71289974876e8
Andy Heggs <Andy@Heggs.net>
2024-08-30 11:46:23 +0100

    superuser added

--- project/settings.py
+++ project/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -74,6 +75,7 @@ CORS_ALLOWED_ORIGINS=[
     'https://assetec-15d8b96cc456.herokuapp.com'
 ]
 
+CSRF_TRUSTED_ORIGINS = ['your_heroku_url_here', 'http://127.0.0.1:8000']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
